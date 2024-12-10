1e21617c697e540b987f69c7f1e6ffdc046d517c
Lukas Seeger <lseeger@3-H-7.42heilbronn.de>
2024-12-10 13:05:37 +0100

    Makefile Setup

---
+++ include/push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lseeger <lseeger@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 12:47:27 by lseeger           #+#    #+#             */
+/*   Updated: 2024/12/10 13:01:55 by lseeger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/get_next_line/get_next_line.h"
+# include "../libft/libft/libft.h"
+# include "../libft/printf/include/ft_printf.h"
+
+#endif
