febafd7872305d20e5f5d6320c0882a525c1a0dc
Lukas Seeger <lseeger@3-F-1.42heilbronn.de>
2025-01-13 16:34:23 +0100

    Last fix to handle max-int values

--- libft/libft/ft_aisi.c
+++ libft/libft/ft_aisi.c
@@ -6,23 +6,43 @@
 /*   By: lseeger <lseeger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:11:20 by lseeger           #+#    #+#             */
-/*   Updated: 2024/12/16 14:54:50 by lseeger          ###   ########.fr       */
+/*   Updated: 2025/01/13 16:28:48 by lseeger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static bool	check_limits(long long result, bool is_negative)
+{
+	if (is_negative && -result < INT_MIN)
+		return (false);
+	else if (!is_negative && result > INT_MAX)
+		return (false);
+	return (true);
+}
+
 bool	ft_aisi(char *str)
 {
+	long long	result;
+	bool		is_negative;
+
+	result = 0;
+	is_negative = false;
 	if (!str)
 		return (false);
 	while (*str == ' ')
 		str++;
 	if (*str == '-')
+	{
+		is_negative = true;
 		str++;
+	}
 	while (*str)
 	{
 		if (!ft_isdigit(*str))
+			return (false);
+		result = result * 10 + *str - '0';
+		if (!check_limits(result, is_negative))
 			return (false);
 		str++;
 	}
